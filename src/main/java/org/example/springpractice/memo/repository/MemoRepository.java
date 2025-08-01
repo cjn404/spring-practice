b180e140cc5e6ed5646148d56013be305f67a148
cjn404 <moi404@naver.com>
2025-08-01 09:42:18 +0900

    refactor: clean up unused imports in memo

--- src/main/java/org/example/springpractice/memo/repository/MemoRepository.java
+++ src/main/java/org/example/springpractice/memo/repository/MemoRepository.java
@@ -2,7 +2,6 @@ package org.example.springpractice.memo.repository;
 
 import org.example.springpractice.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
 }
